0f6179c1ab54a2083e8fd630180c16a9cd366416
Lazuli_J <88023413+LazuliJ@users.noreply.github.com>
2024-08-28 15:11:50 +0900

    Create MINCO_DFS_tower_7.cpp

---
+++ MINCO_DFS_tower_7.cpp
@@ -0,0 +1,35 @@
+// 민코딩 DFS의 탑 7번
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string target; // 통과해야 하는 문자열
+string tars[2]; // 악마의 다리, 천사의 다리
+int ans = 0; // 가지수
+
+void DFS(int cur, int leng, int mode) {
+	if (cur == target.length()) {
+		ans++;
+		return;
+	}
+	if (leng == tars[mode].length()) {
+		return;
+	}
+
+	DFS(cur, leng + 1, mode);
+	if (tars[mode][leng] == target[cur]) {
+		if (mode == 0) mode = 1;
+		else mode = 0;
+		DFS(cur + 1, leng + 1, mode);
+	}
+}
+
+int main() {
+	cin >> target;
+	cin >> tars[0]; cin >> tars[1];
+	DFS(0, 0, 0);
+	DFS(0, 0, 1);
+	cout << ans;
+}
